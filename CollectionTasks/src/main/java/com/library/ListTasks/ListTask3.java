a5b743b23a84c03ea73d3b87f53b4a7f6af59a31
Vasundhara Adla <vasundhara.adla@gmail.com>
2025-03-27 19:36:03 +0530

    List Task3 done

---
+++ CollectionTasks/src/main/java/com/library/ListTasks/ListTask3.java
@@ -0,0 +1,17 @@
+package com.library.ListTasks;
+import java.util.*;
+public class ListTask3 {
+    public static void main(String[] args) {
+        List<Integer> list1=new ArrayList<>(Arrays.asList(1,2,4,8,3,4,9));
+        List<Integer> list2=new ArrayList<>(Arrays.asList(11,5,4,7,3,2,9));
+        List<Integer> list3=new ArrayList<>(Arrays.asList(1,2,4,8,3,4,9));
+        System.out.println(list1.equals(list2));
+        System.out.println(list1.equals(list3));
+        System.out.println("List1: "+list1);
+        System.out.println("List2: "+list2);
+        list1.addAll(list2);
+        System.out.println("Joined list: "+list1);
+        List<Integer> clonedList=new ArrayList<>(list1);
+        System.out.println("Cloned List: "+clonedList);
+    }
+}
